e7c8495c6cc62a067efe8fc4debe9e8a7687c277
Zeqiang-Lai <916323301@qq.com>
2019-05-29 09:22:33 +0800

    code refactoring

--- src/main/ParserRunner.java
+++ src/main/ParserRunner.java
@@ -21,12 +21,12 @@ import java.util.Vector;
 
 public class ParserRunner {
 
-    static String readFile(String path, Charset encoding) throws IOException {
+    String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
     }
 
-    static Vector<Token> readTokensFromXML(String path) {
+    Vector<Token> readTokensFromXML(String path) {
         Vector<Token> tokens = new Vector<>();
 
         String xml = null;
@@ -44,30 +44,10 @@ public class ParserRunner {
         return tokens;
     }
 
-    static void printUsage() {
-        String usage = "OVERVIEW: ZQC compiler\n\n" +
-                "USAGE: lexer [options] <inputs>\n\n" +
-                "OPTIONS:\n" +
-                "\t-xml    \tUse xml as input.\n" +
-                "\t-o <file>\tWrite output to <file>";
-        System.out.println(usage);
-    }
-
-    public static void main(String[] args) {
-        String source_path;
-        String output_name;
-
-        if(args.length == 0) {
-            printUsage();
-            return;
-        } else {
-            source_path = args[0];
-            for(int i=0; i<args.length; ++i) {
-
-            }
-        }
+    void run(String source_path, String out, boolean use_xml) {
         File f = new File(source_path);
-
+        String file_name = f.getName();
+        if(out == null) out = file_name;
 
         String source = null;
         try {
@@ -76,21 +56,21 @@ public class ParserRunner {
             e.printStackTrace();
         }
 
-
         ErrorCollector errorCollector = ErrorCollector.getInstance();
-        errorCollector.setFile_name(f.getName());
+        errorCollector.setFile_name(file_name);
 
-        SourceBuffer buff = new SourceBuffer(source);
-        Lexer lexer = new Lexer(buff);
         Vector<Token> tokens = new Vector<>();
-        Token token = lexer.scan();
-        while (token.type != TokenType.EOF) {
-            tokens.add(token);
-            token = lexer.scan();
+        if(use_xml) {
+            tokens = readTokensFromXML(source_path);
+        } else {
+            SourceBuffer buff = new SourceBuffer(source);
+            Lexer lexer = new Lexer(buff);
+            Token token = lexer.scan();
+            while (token.type != TokenType.EOF) {
+                tokens.add(token);
+                token = lexer.scan();
+            }
         }
-
-//        tokens = readTokensFromXML("test/test1.c.xml");
-
 
         Parser parser = new Parser(tokens);
         StmtNode.CompilationUnit tree = parser.parse();
@@ -100,16 +80,55 @@ public class ParserRunner {
             return;
         }
 
+        System.out.println(file_name + " is successfully parsed!");
         XMLPrinter printer = new XMLPrinter();
         String xml = printer.print(tree);
 
-        PrintWriter out;
+        PrintWriter writer;
         try {
-            out = new PrintWriter(source_path+".tree.xml");
-            out.println(xml);
-            out.close();
+            writer = new PrintWriter(source_path+"_tree.xml");
+            writer.println(xml);
+            writer.close();
+            System.out.println("AST in XML format is saved at " + source_path + "_tree.xml.");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    static void printUsage() {
+        String usage = "OVERVIEW: ZQC compiler\n\n" +
+                "USAGE: lexer [options] <inputs>\n\n" +
+                "OPTIONS:\n" +
+                "\t-xml    \tUse xml as input.\n" +
+                "\t-o <file>\tWrite output to <file>.xml";
+        System.out.println(usage);
+    }
+
+    public static void main(String[] args) {
+        String source = null;
+        String out = null;
+        boolean use_xml = false;
+
+        if(args.length == 0) {
+            printUsage();
+            return;
+        } else {
+            for(int i=0; i<args.length;) {
+                if(args[i].equals("-o")) {
+                    out = args[i+1];
+                    i++;
+                } else if(args[i].equals("-xml")) {
+                    use_xml = true;
+                    i++;
+                } else {
+                    if(source == null)
+                        source = args[i];
+                    i++;
+                }
+            }
+        }
+
+        ParserRunner runner = new ParserRunner();
+        runner.run(source, out, use_xml);
     }
 }
